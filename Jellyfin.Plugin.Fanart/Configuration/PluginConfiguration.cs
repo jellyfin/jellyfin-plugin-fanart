72e6b1c7fde4b26b109b36ff58e17f4dbefed5e7
crobibero <cody@robibe.ro>
2020-03-24 08:06:26 -0600

    Fix Build

--- Jellyfin.Plugin.Fanart/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Fanart/Configuration/PluginConfiguration.cs
@@ -5,5 +5,6 @@ namespace Jellyfin.Plugin.Fanart.Configuration
     public class PluginConfiguration : BasePluginConfiguration
     {
         public string PersonalApiKey { get; set; }
+        public const int DefaultBufferSize = 4096;
     }
 }
