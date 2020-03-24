b0b336347d174dd3a9e30d9c41c3cefe0dae2645
crobibero <cody@robibe.ro>
2020-03-24 17:04:18 -0600

    Move to proper defaults

--- Jellyfin.Plugin.Fanart/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Fanart/Configuration/PluginConfiguration.cs
@@ -5,6 +5,5 @@ namespace Jellyfin.Plugin.Fanart.Configuration
     public class PluginConfiguration : BasePluginConfiguration
     {
         public string PersonalApiKey { get; set; }
-        public const int DefaultBufferSize = 4096;
     }
 }
